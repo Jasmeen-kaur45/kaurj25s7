a47bbe2da61e92a31cc8939dfe0d659124abc011
Jasmeen-kaur45 <gurnajasmeen59@gmail.com>
2025-08-01 21:28:38 -0400

    defined EvilCircle and controlled its movement

--- assignment4/part4/main.js
+++ assignment4/part4/main.js
@@ -87,6 +87,33 @@ class Ball extends shape {
   }
 }
 
+//definign evilcircle
+class EvilCircle extends shape{
+    constructor(x,y){
+        super(x,y,20,20);
+        this.color="white";
+        this.size=10;
+
+        //moment controls
+        window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "a":
+      this.x -= this.velX;
+      break;
+    case "d":
+      this.x += this.velX;
+      break;
+    case "w":
+      this.y -= this.velY;
+      break;
+    case "s":
+      this.y += this.velY;
+      break;
+  }
+});
+    }
+}
+
 const balls = [];
 
 while (balls.length < 25) {
